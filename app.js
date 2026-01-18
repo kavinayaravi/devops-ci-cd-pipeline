a696ca5261bf36f137365826e98a1438332fbc7e
kavinayaravi <rkavinaya2005@gmail.com>
2026-01-18 22:26:54 +0530

    Update app response to v2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Pipeline Working');
+  res.send('DevOps Pipeline Working v2 🚀');
 });
 
 app.get('/metrics', async (req, res) => {
