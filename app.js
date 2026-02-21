4656c7348fdc824bd761b79606c264ea2f84f84f
kavinayaravi <rkavinaya2005@gmail.com>
2026-02-21 21:29:49 +0530

    auto-deploy-test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Pipeline Working v2 🚀');
+  res.send('DevOps Pipeline Working 🚀');
 });
 
 app.get('/metrics', async (req, res) => {
